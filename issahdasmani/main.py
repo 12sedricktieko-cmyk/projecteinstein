8fe21b0ed9fe6889e27d2675f7bce7ed314a5e07
ruuhullah17-maker <ruuhullah17@gmail.com>
2026-03-31 17:44:38 +0000

    added a new code

--- issahdasmani/main.py
+++ issahdasmani/main.py
@@ -15,3 +15,13 @@ data_types()
 def transport_types():
     return("trotro,uber,taxi,metro mass,okada")
 print(transport_types())
+
+first_name = "King"
+last_name = "Ruuhullah"
+full_name = first_name+ " " +last_name
+print(full_name)
+
+def user_name():
+    name = input("Enter your name:")
+    print("your name is" + name)
+user_name()
